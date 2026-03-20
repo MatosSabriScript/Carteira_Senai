ef772bd8539988f24332c112f4aef32f09f96ce8
Sabrina Matos Almeida <sabrinamatosalmeida2@gmail.com>
2026-03-20 11:04:46 -0300

    Feito o app, falta estilizar e ver se o QR code funciona 23/02/26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 }
 
 dependencies {
+    implementation("com.google.zxing:core:3.5.4")
+    implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -53,4 +55,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
